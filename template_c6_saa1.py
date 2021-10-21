596e96ea2f7ffb8341f3f2643522df718929f4e7
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-21 11:39:57 +0530

    Create template_c6_saa1.py

---
+++ template_c6_saa1.py
@@ -0,0 +1,13 @@
+# Create a list 'numbers'
+
+
+# Create a variable 'count' 
+# Initialize it to '0'
+
+
+# Create a 'for' loop to access every element in the list
+
+  # Increment 'count' by '1'
+  
+
+# Print the value of 'count'
